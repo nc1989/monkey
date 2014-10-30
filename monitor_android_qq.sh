1503d7d14f63e8fcddaefcb6751c7f159012293a
Yanxiong <Yanxiong.hu@dianping.com>
2014-10-30 21:30:05 +0800

    monitor脚本对启动失败的判断.

--- monitor_android_qq.sh
+++ monitor_android_qq.sh
@@ -36,10 +36,11 @@ monitor_adb()
 main()
 {
 	err1="Error sending touch event"
-	err2="java.net.SocketException: Broken pipe"
-	err3="Script terminated due to an exception"
-	err4="Error sending drag start event"
-	err5="Exception in thread"
+	err2="Error sending drag start event"
+	# err2="java.net.SocketException: Broken pipe"
+	# err3="Script terminated due to an exception"
+	# err5="Exception in thread"
+	err6="Error : failed to start Android QQ daemon"
 	echo ">>> Info : try to start android qq !" > ${logfile}
 	echo ">>> Info : try to start android qq !" > ${errfile}
 	start_qq_success=0
@@ -47,7 +48,7 @@ main()
 	while [ ${start_qq_success} -eq 0 ]
 	do
 		# grep "${err1}" ${errfile}
-		grep -E "${err1}|${err2}|${err3}|${err4}|${err5}" ${errfile}
+		grep -E "${err1}|${err2}|${err6}" ${errfile}
 		if [ $? -eq 0 ];then
 			echo "\n>>> Info : try again !"
 		    start_android_qq
