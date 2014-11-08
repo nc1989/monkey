55be38982420e7d60de4a235d1553c97d06dc468
zhaoqifa <qifa.zhao@gmail.com>
2014-11-08 13:05:23 +0800

    根据位置查找群失败的时候继续在附近的屏幕查找

--- lib/agent.py
+++ lib/agent.py
@@ -403,46 +403,92 @@ class Agent(object):
 
     def get_group_name_id(self):
         #在GROUP_CHAT界面时，获取group name和id
+        logger.info("获取当前群的名字和id")
         if not self.goto('GROUP_INFO'):
             return None, None
         group_name, group_id = self.extract_group_info()
+        logger.info("获取到群的名字和id为: %s,%s", to_str(group_name), group_id)
         self.goto('GROUP_CHAT')
         return group_name, group_id
 
     def update_groups(self, gname, gid, drag, pos):
         self.groups[gid] = Group(gname, gid, drag, pos)
 
-    def enter_group_by_postion(self, gid):
-        group = self.groups[gid]
-        drag, pos = group.drag, group.pos
+    def enter_group_by_postion(self, gid, drag, pos):
+        logger.info("根据位置进群[%s]，drag/pos=%s/%s", gid, drag, pos)
         self.drag(drag)
         if not self.switch_by_pixel('GROUP_LIST', 'GROUP_CHAT',
                                     HORIZON_MID, pos):
             return False
         group_name, group_id = self.get_group_name_id()
         if group_id == gid:  # 成功返回True
+            logger.info("群号匹配成功")
             return True
+        logger.warning("群号匹配不成功，%s != %s", group_id, gid)
 
         if group_name and group_id:
             # 失败的话，更新groups信息，防止下次继续出错
             self.update_groups(group_name, group_id, drag, pos)
+        self.goto("GROUP_LIST")  # 进群失败，退回到群列表页
         return False
 
-    def enter_group_by_finding(self, gid):
+    def enter_group_in_screen(self, gname, gid):
+        #解析并验证当前屏幕上的群组
+        logger.info("解析当前屏幕所有群")
+        screen_groups = self.extract_groups()
+        for name, pos in screen_groups:
+            if str_equal(name, gname):
+                logger.info("发现名字[%s]匹配的群，进入验证群号", to_str(name))
+                if self.enter_group_by_postion(gid, 0, pos):
+                    logger.info("恭喜，进群成功")
+                    return True
+        logger.info("当前屏幕没有找到指定群[%s,%s]", gid, to_str(gname))
+        return False
+
+    def enter_group_by_finding(self, current_drag, gid):
+        if not self.goto("GROUP_LIST"):
+            return
+        gname = self.groups[gid].name
+        logger.info("尝试查找并进入群[%s,%s]", gid, to_str(gname))
+        if self.enter_group_in_screen(gname, gid):
+            return 0
+
+        #当前屏幕没有找到，往上翻一页去找
+        #如果当前屏幕是第一屏，就没有这个必要了
+        if current_drag != 0:
+            logger.info("往前翻1屏，继续查找")
+            self.drag(-1)
+            if self.enter_group_in_screen(gname, gid):
+                return 0
+
+        #如果当前屏幕的前一屏幕也没有找到，进入后一屏去找
+        #如果之前往上翻过一屏了，现在需要往下翻两屏
+        drag = 2 if current_drag != 0 else 1
+        logger.info("往后翻%s屏，继续查找", gid)
+        self.drag(drag)
+        if self.enter_group_in_screen(gname, gid):
+            return 0
+
+        logger.error("前后1屏都没找到对应群，累死了，不找了")
         return 1
 
     def enter_group(self, gid):
+        logger.info("准备进入群[%s]", gid)
         if gid not in self.groups:
-            logger.error("can not enter a unknown group!")
-            return 1 # 暂时不接受进入无记录群的需求
+            logger.error("群列表中没有该群的登记信息[%s]，不能进", gid)
+            return 1  # 暂时不接受进入无记录群的需求
         if not self.goto('CONTACTS'):
             return 2
         if not self.goto('GROUP_LIST'):
             return 3
         #前面两步操作保证进入的GROUP_LIST页面是没有被向下翻页过的
-        if self.enter_group_by_postion(gid):
+        group = self.groups[gid]
+        drag, pos = group.drag, group.pos
+        if self.enter_group_by_postion(gid, drag, pos):
+            logger.info("根据位置进群[%s]成功", gid)
             return 0
-        return self.enter_group_by_finding(gid)
+        logger.info("根据位置进群[%s]失败", gid)
+        return self.enter_group_by_finding(drag, gid)
 
     def wait_screen(self, expect_screen):
         # 等待模拟器跳转到某个页面
@@ -513,9 +559,21 @@ if __name__ == "__main__":
     #print agent.goto('CONTACTS')
     #print agent.goto_device_home()
     #agent.gen_groups()
-    #print agent.enter_group("301430156")
+    print agent.enter_group("301430156")
+    print agent.enter_group("326027223")
+    print agent.enter_group("390048779")
+    print agent.enter_group("330635757")
+    print agent.enter_group("361394670")
+    print agent.enter_group("87646463")
+    print agent.enter_group("246319397")
+    print agent.enter_group("60588793")
+    print agent.enter_group("159643583")
+    print agent.enter_group("95627741")
+    print agent.enter_group("384048199")
+    print agent.enter_group("295024743")
+    print agent.enter_group("86904231")
     #for i in xrange(50):
     #    print agent.send_group_msg("天命，哈哈哈%s" % i, False)
-    print agent.check_group_msg('不要打岔')
-    print agent.check_group_msg('挺实惠的啊')
-    print agent.check_group_msg('挺实惠的啊123123')
+    #print agent.check_group_msg('不要打岔')
+    #print agent.check_group_msg('挺实惠的啊')
+    #print agent.check_group_msg('挺实惠的啊123123')
