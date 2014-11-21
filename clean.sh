5977ed22772a9e5ba8b853d2c3d9958c1c3ca780
chris <yanxiong.hu@dianping.com>
2014-11-21 17:34:11 +0800

    clean.sh add uninstal and install qq

--- clean.sh
+++ clean.sh
@@ -1,9 +1,9 @@
-#!/bin/bash 
+#!/bin/bash
 
 function start_emulator
 {
     for((i=1;i<=8;i++))
-    do 
+    do
         pid=$(ps -ef|grep emulator|grep "S${i}"|awk '{print $2}')
         kill -9 ${pid}
         sleep 2
@@ -20,9 +20,22 @@ function reinstall_qq
     adb -s ${device} uninstall com.tencent.mobileqq
     sleep 2
     adb -s ${device} install qq_apk/qq_v5.1.1.2245.apk
+	sleep 2
+	adb -s ${device} shell input keyevent 82
+	adb -s ${device} shell input keyevent KEYCODE_HOME
+	sleep 1
+	adb -s ${device} shell am start -n com.tencent.mobileqq/com.tencent.mobileqq.activity.SplashActivity
+	sleep 5
+	adb -s ${device} shell input keyevent KEYCODE_BACK
 }
 
 function reinstall_all
 {
-    devices=$(adb devices|awk '{print $1}')
+    devices=$(adb devices|grep '^emulator'|awk '{print $1}')
+	for device in ${devices}
+	do
+		reinstall_qq ${device}
+	done
 }
+
+reinstall_all
