2938b5a9ba95b181e2f1f45028920be21e7622c4
Yanxiong <Yanxiong.hu@dianping.com>
2014-11-21 14:10:00 +0800

    批量启动模拟器.

---
+++ startEmulators.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+
+import os
+import sys
+import time
+import logging
+import simplejson as json
+
+def restart_adb():
+    os.system("adb kill-server")
+    os.system("adb devices")
+    time.sleep(5)
+
+def restart_emulator(device):
+    print ">>> device", device
+	os.system("emulator -avd %s &" % device)
+    time.sleep(20)
+
+if __name__ == '__main__':
+    print ">>> start the emulators..."
+    for i in xrange(1,10):
+        restart_emulator('S%s' % i)
+
