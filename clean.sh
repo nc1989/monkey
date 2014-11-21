d559ecc48080415e5d3da68fd3c13ba92c37db8e
zhaoqifa <qifa.zhao@gmail.com>
2014-11-21 17:18:52 +0800

    增加一个批量重启emulator和重装QQ的脚本

---
+++ clean.sh
@@ -0,0 +1,28 @@
+#!/bin/bash 
+
+function start_emulator
+{
+    for((i=1;i<=8;i++))
+    do 
+        pid=$(ps -ef|grep emulator|grep "S${i}"|awk '{print $2}')
+        kill -9 ${pid}
+        sleep 2
+        emulator -avd "S${i}" &
+        sleep 2
+    done
+}
+
+function reinstall_qq
+{
+    device=$1
+    adb -s ${device} shell am force-stop com.tencent.mobileqq
+    sleep 2
+    adb -s ${device} uninstall com.tencent.mobileqq
+    sleep 2
+    adb -s ${device} install qq_apk/qq_v5.1.1.2245.apk
+}
+
+function reinstall_all
+{
+    devices=$(adb devices|awk '{print $1}')
+}
