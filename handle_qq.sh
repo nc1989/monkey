2464205d6962065ab11538c899c4c205a796653b
chris <yanxiong.hu@dianping.com>
2014-11-26 20:10:35 +0800

    将stop和start放到一个handle_qq.sh中

--- handle_qq.sh
+++ handle_qq.sh
@@ -1,26 +1,34 @@
 #!/bin/bash
 
+function stop_qq
+{
+    device=$1
+	adb -s ${device} shell am force-stop com.tencent.mobileqq
+	sleep 2
+}
+
 function start_qq
 {
     device=$1
 	adb -s ${device} shell input keyevent KEYCODE_MENU
 	adb -s ${device} shell input keyevent KEYCODE_BACK
 	adb -s ${device} shell am start -n com.tencent.mobileqq/com.tencent.mobileqq.activity.SplashActivity
-	sleep 5
+	sleep 10
 	adb -s ${device} shell input keyevent KEYCODE_BACK
 	adb -s ${device} shell input keyevent KEYCODE_BACK
 	adb -s ${device} shell input keyevent KEYCODE_BACK
 	adb -s ${device} shell am start -n com.tencent.mobileqq/com.tencent.mobileqq.activity.SplashActivity
 }
 
-function start_all_qq
+function handle_all_qq
 {
+	cmd=$1_qq
     devices=$(adb devices|grep '^emulator'|awk '{print $1}')
 	for device in ${devices}
 	do
-		start_qq ${device} &
+		$cmd ${device} &
 	done
 	wait
 }
 
-start_all_qq
+handle_all_qq $1
