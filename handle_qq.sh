6ef7637ea74564de93fca7d355e8f31cd8e0a6f9
chris <yanxiong.hu@dianping.com>
2014-12-05 18:20:54 +0800

    使用xterm和fab来启动模拟器和QQ

--- handle_qq.sh
+++ handle_qq.sh
@@ -2,36 +2,53 @@
 
 function stop_qq
 {
-    device=$1
-	adb -s ${device} shell am force-stop com.tencent.mobileqq
-	sleep 2
+    deviceid=$1
+	port=${deviceid:0-4:4}
+	device='S'$[ ( port - 5552 ) / 2 ]
+	echo $deviceid
+	echo $device
+	adb -s ${deviceid} shell am force-stop com.tencent.mobileqq
+	ps -ef | grep 'emulator' | grep -v grep | grep $device | awk '{print $2}' | xargs kill
 }
 
 function start_qq
 {
-    device=$1
-	adb -s ${device} shell input keyevent KEYCODE_MENU
-	adb -s ${device} shell input keyevent KEYCODE_BACK
-	adb -s ${device} shell am start -n com.tencent.mobileqq/com.tencent.mobileqq.activity.SplashActivity
+    deviceid=$1
+	port=${deviceid:0-4:4}
+	device='S'$[ ( port - 5552 ) / 2 ]
+	echo $deviceid
+	echo $device
+	#emulator -avd $device -port $port &
+	export DISPLAY=":0.0"
+	xterm -display $DISPLAY -e "emulator -avd $device -port $port" &
 	sleep 60
-	adb -s ${device} shell input keyevent KEYCODE_BACK
-	adb -s ${device} shell input keyevent KEYCODE_BACK
-	adb -s ${device} shell input keyevent KEYCODE_BACK
-	adb -s ${device} shell am start -n com.tencent.mobileqq/com.tencent.mobileqq.activity.SplashActivity
+	adb -s ${deviceid} shell input keyevent KEYCODE_MENU
+	adb -s ${deviceid} shell input keyevent KEYCODE_BACK
+	adb -s ${deviceid} shell am start -n com.tencent.mobileqq/com.tencent.mobileqq.activity.SplashActivity
+	sleep 30
+	adb -s ${deviceid} shell input keyevent KEYCODE_BACK
+	adb -s ${deviceid} shell input keyevent KEYCODE_BACK
+	adb -s ${deviceid} shell input keyevent KEYCODE_BACK
+	adb -s ${deviceid} shell am start -n com.tencent.mobileqq/com.tencent.mobileqq.activity.SplashActivity
 }
 
 function handle_qqs
 {
 	cmd=$1_qq
+	shift
 	if [[ $# -eq 1 ]];then
-		devices=$(adb devices|grep '^emulator'|awk '{print $1}')
-		for device in ${devices}
-		do
-			$cmd ${device} &
-		done
-	else
-		deviceid=emulator-$2
+		deviceid=emulator-$1
+		echo $deviceid
 		$cmd $deviceid &
+	else
+		deviceids=$@
+#		devices=$(adb devices|grep '^emulator'|awk '{print $1}')
+		for deviceid in $deviceids
+		do
+			echo $deviceid
+			$cmd 'emulator-'$deviceid &
+			sleep 10
+		done
 	fi
 	wait
 }
