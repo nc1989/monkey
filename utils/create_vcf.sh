89c4615e52ce809c06660ec96db1c51dcce242a5
zhaoqifa <qifa.zhao@gmail.com>
2014-12-31 15:26:55 +0800

    增加一个脚本，用来分割电话号码list

---
+++ utils/create_vcf.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+function create_vcf
+{
+    file=$1
+    begin=$2
+    end=$3
+    while read line
+    do
+        echo "BEGIN:VCARD"
+        echo "VERSION:3.0"
+        echo "N:$line;;;;"
+        echo "TEL;TYPE=cell:$line"
+        echo "END:VCARD"
+    done <<< "$(sed -n "${begin},${end}p" $file)"
+}
+
+function import_contacts
+{
+    index=$1
+    lines=100
+    for((i=0;i<$2;i++))
+    do
+        emu_index=$((5554+2*i))
+        echo $(($index+$i*$lines)) $(($index+$i*$lines+$lines-1)) 
+        rm -f contacts.vcf
+        create_vcf tel $(($index+$i*$lines)) $(($index+$i*$lines+$lines-1)) >> contacts.vcf
+        adb -s emulator-"$emu_index" push contacts.vcf /sdcard/contacts.vcf
+    done
+}
+function main
+{
+    main_index=$1
+    import_contacts $main_index 5
+}
+
+main $@
