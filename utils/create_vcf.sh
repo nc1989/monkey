f5f9ef762f65a93c8ef787ed3659d21b08c4085f
zhaoqifa <qifa.zhao@gmail.com>
2015-01-07 20:16:28 +0800

    update create_vcf，增加通讯录清除和导入的工作

--- utils/create_vcf.sh
+++ utils/create_vcf.sh
@@ -18,14 +18,17 @@ function create_vcf
 function import_contacts
 {
     index=$1
-    lines=100
+    lines=40
     for((i=0;i<$2;i++))
     do
         emu_index=$((5554+2*$i))
         echo $(($index+$i*$lines)) $(($index+$i*$lines+$lines-1)) 
         rm -f contacts.vcf
         create_vcf tel $(($index+$i*$lines)) $(($index+$i*$lines+$lines-1)) >> contacts.vcf
+        adb -s emulator-$device shell pm clear com.android.providers.contacts
         adb -s emulator-"$emu_index" push contacts.vcf /sdcard/contacts.vcf
+        sleep 3
+        adb -s emulator-$device shell am start -t "text/x-vcard" -d "file:///sdcard/contacts.vcf" -a android.intent.action.VIEW com.android.contacts
     done
 }
 function main
